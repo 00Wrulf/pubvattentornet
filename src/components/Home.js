6c621c4bdcb88492fb88c55a142b354d7a8bc881
Daniel Wärulf <danne.warulf@icloud.com>
2022-09-29 14:05:36 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ class Home extends Component {
     return (
       <div className='welcomeContainer'>
         <div className='welcome'>
-          <p>Lennings gata 2, Norrköping</p>
+          <p>Lennings gata 2</p>
           <h1>Pub Vattentornet</h1>
           <div id='arrow'>
             <svg width='22' height='90' viewBox='0 0 22 90' fill='none' xmlns='http://www.w3.org/2000/svg' opacity='1'>
