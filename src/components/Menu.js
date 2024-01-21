b081041700847ea243fdb3c833d68cce52c3536c
Samuel Rothin <samuel.rothin@gmail.com>
2024-01-21 15:45:18 +0100

    Added loading indicator

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/menu.css'
+import Spinner from '../images/Spinner.gif'
 
 const Menu = () => {
 
     const [menuItems, setMenuItems] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // Fetch data from the API
         fetch(process.env.REACT_APP_MENU_API_URL)
             .then((response) => response.json())
-            .then((data) => setMenuItems(data))
+            .then((data) => {
+                setMenuItems(data)
+                setLoading(false)
+            })
             .catch((error) => console.error('Error fetching menu:', error));
     }, []);
 
@@ -31,7 +36,10 @@ const Menu = () => {
     return (
         <div className='menu'>
             <h1>Menu</h1>
-            {Object.keys(organizedMenu).map((category) => (
+            {loading ? <div className='loadingSpinnerContainer'>
+            <svg width="48" height="48" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" class="spinner_aj0A"/></svg>
+            </div> : 
+            Object.keys(organizedMenu).map((category) => (
                 <div key={category}>
                     <h2>{category}</h2>
                     <ul className='menuList'>
